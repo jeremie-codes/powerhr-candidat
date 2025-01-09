2f2356ddc5a07cbe24652ba3be0b44a49b961bbf
Sal Aime <salemnk02@gmail.com>
2025-01-09 11:03:01 +0100

    add meta description

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,12 +4,30 @@
 
 <head>
     <meta charset="utf-8">
-    <title>@yield('title') | {{config('app.name')}}</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <meta content="Minimal Admin & Dashboard Template" name="description">
-    <meta content="Themesdesign" name="author">
-    <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ URL::asset('build/images/favicon.ico') }}">
+    <title>@yield('title') | {{ config('app.name') }}</title>
+
+    <meta name="theme-color" content="#888">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="">
+    <meta name="description"
+        content="Tapez votre mot-clé, puis cliquez sur Rechercher pour trouver votre emploi idéal. Peu importe le secteur, nous avons des offres adaptées à vos compétences." />
+    <meta name="keywords"
+        content="emploi, travail, offres d'emploi, trouver un emploi, carrière, recrutement, job, recherche d'emploi, domaine de compétence" />
+    <link rel="canonical" href="#">
+    <meta property="og:type" content="website" />
+    <meta property="og:title"
+        content="Trouvez l'emploi qui correspond à votre vie - Plateforme de recherche d'emploi" />
+    <meta property="og:description"
+        content="Peu importe votre secteur, trouvez l'emploi qui correspond à vos compétences. Recherchez dès maintenant votre poste idéal." />
+    <meta property="og:image" content="{{ asset('build/images/logo.png') }}" />
+    <meta property="og:url" content="#" />
+    <meta property="og:image:width" content="1024" />
+    <meta property="og:image:height" content="1024" />
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:image" content="{{ asset('build/images/logo.png') }}">
+
+    <link rel="shortcut icon" href="{{ URL::asset('images/favicon/favicon.png') }}">
 
     @include('layouts.head-css')
     <!-- Styles -->
@@ -27,15 +45,15 @@
         <!-- page wrapper -->
         @include('layouts.page-wrapper')
 
-            <div class="container-fluid group-data-[content=boxed]:max-w-boxed mx-auto">
-                <!-- content -->
-                @yield('content')
-            </div>
+        <div class="container-fluid group-data-[content=boxed]:max-w-boxed mx-auto">
+            <!-- content -->
+            @yield('content')
         </div>
-        <!-- End Page-content -->
-        <!-- footer -->
-        @include('layouts.footer')
     </div>
+    <!-- End Page-content -->
+    <!-- footer -->
+    @include('layouts.footer')
+</div>
 </div>
 <!-- end main content -->
 @stack('modals')
